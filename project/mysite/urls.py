f2be60ddc52be4a1f2affa800c7f2e37f1ad5eab
dimvelen <dimvelen@mail.ru>
2022-09-06 15:59:07 +0700

    Changed the HTML for the site.

--- project/mysite/urls.py
+++ project/mysite/urls.py
@@ -19,11 +19,12 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+urlpatterns =static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT),
 
-   ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = [
+      path('admin/', admin.site.urls),
+      path('', include('blog.urls')),
+   ] 
 
 
 
