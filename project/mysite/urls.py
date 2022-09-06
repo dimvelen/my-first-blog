bda1941f4bd4a65d87125bd8a6e22fa27510e34a
dimvelen <dimvelen@mail.ru>
2022-09-06 11:02:47 +0700

    Changed the HTML for the site.

--- project/mysite/urls.py
+++ project/mysite/urls.py
@@ -16,7 +16,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
    ]
+
+
+""" if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+                      path('__debug__/', include(debug_toolbar.urls)),
+                  ] + urlpatterns
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+"""
