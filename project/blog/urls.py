287cbce787931f9cf4f7fcea2f384089b40815a9
dimvelen <dimvelen@mail.ru>
2022-09-06 11:58:42 +0700

    Changed the HTML for the site.

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 urlpatterns = [
     path('', views.post_list, name='post_list'),
 ]
